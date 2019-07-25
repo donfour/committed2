d0f0faf291b520f352db4bf6deb9721726f262d3
donfour <donovanso@yahoo.com.hk>
2019-07-25 22:57:22 +0800

    replaced App.css with createGlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-// module dependencies
+// external dependencies
 import React, { Component } from 'react';
-// styles
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
 // components
 import TodoInput from './components/TodoInput';
 import TodoList from './containers/TodoList';
 // react contexts
 import { TodoProvider } from './context/TodoContext';
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root, .App{
+    height: 100%;
+  }
+
+  *{
+    font-family: 'Lato', sans-serif;
+  }
+
+  .App{
+    margin: auto;
+    width: 700px;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
 
 export default class App extends Component {
   
@@ -23,6 +42,7 @@ export default class App extends Component {
   render(){
     return (
       <TodoProvider>
+        <GlobalStyle/>
         <div className="App">
           <TodoInput/>
           <TodoList/>
