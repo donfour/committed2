49dd18ed77945897218e0a7aa75306fb61627517
donfour <donovanso@yahoo.com.hk>
2019-08-31 15:14:17 +0800

    fixed todo input width problem

--- src/components/TodoInput/todo-input.style.js
+++ src/components/TodoInput/todo-input.style.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export const TodoInputWrapper = styled.input`
     border: none;
+    padding: 0 0 2px;
     color: ${({theme}) => theme.primary};
     border-bottom: 2px solid ${({theme}) => theme.primary};
     width: 100%;
     font-size: 36px;
     letter-spacing: 1px;
     margin-top: 200px;
-    padding-left: 5px;
     &:focus{
         outline: none;
     }
